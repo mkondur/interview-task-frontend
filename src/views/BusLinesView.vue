de61e94ced3b7600994096092ecfdc2416321b76
Mykola Kondur <mykola@propergate.co>
2024-06-06 22:57:03 +0200

    Add BusLines & BusStops views

---
+++ src/views/BusLinesView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Select Bus Line</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ src/views/BusStopsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Bus Stops</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
