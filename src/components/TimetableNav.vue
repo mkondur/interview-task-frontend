a5abe8436c19b778cb83ad7cb15cd42fb76105f6
Mykola Kondur <mykola@propergate.co>
2024-06-06 23:07:44 +0200

    Add TimetableView & TimetableNav

---
+++ src/components/TimetableNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <router-link to="/" class="nav-link" active-class="active"
+          >Bus Lines</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link to="/stops" class="nav-link" active-class="active"
+          >Stops</router-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/views/TimetableView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mt-5">
+    <h1>Timetable</h1>
+    <TimetableNav />
+    <div class="mt-4">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TimetableNav from '../components/TimetableNav.vue'
+</script>
