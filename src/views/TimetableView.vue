72619be648e78e19124b76e84508e5672ae7514d
Mykola Kondur <mykola@propergate.co>
2024-06-07 00:04:05 +0200

    Fetch bus stops, show loading & error state

--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -3,11 +3,23 @@
     <h1>Timetable</h1>
     <TimetableNav />
     <div class="mt-4">
-      <RouterView />
+      <div v-if="loading" class="text-center">Loading...</div>
+      <div v-if="error" class="text-center text-danger">{{ error }}</div>
+      <RouterView v-if="!loading && !error" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import TimetableNav from '../components/TimetableNav.vue'
+import { onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+const loading = computed(() => store.getters.loading)
+const error = computed(() => store.getters.error)
+
+onMounted(() => {
+  store.dispatch('fetchBusStops')
+})
 </script>
