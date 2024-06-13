383188ea164c656e2485c9dbc8a017b4211838a1
Mykola Kondur <mykola@propergate.co>
2024-06-13 20:27:22 +0200

    Style timetable nav

--- src/components/TimetableNav.vue
+++ src/components/TimetableNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav class="rounded-2 px-4 py-3">
     <ul class="nav nav-tabs">
       <li class="nav-item">
         <router-link to="/" class="nav-link" active-class="active"
@@ -14,3 +14,36 @@
     </ul>
   </nav>
 </template>
+
+<style lang="scss" scoped>
+@import '../styles/_variables.scss';
+nav {
+  background-color: $white;
+
+  .nav-tabs {
+    border-bottom: none;
+
+    .nav-link {
+      color: $gray;
+      border: none;
+      font-size: 14px;
+
+      &.router-link-exact-active {
+        color: $dark;
+        font-weight: 500;
+        position: relative;
+
+        &:after {
+          content: '';
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: -16px;
+          height: 2px;
+          background-color: $primary;
+        }
+      }
+    }
+  }
+}
+</style>
