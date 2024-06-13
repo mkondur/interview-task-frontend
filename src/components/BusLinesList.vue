15fbd34deddbb12f2fc1944d50617c8c5e91d228
Mykola Kondur <mykola@propergate.co>
2024-06-13 20:42:10 +0200

    Add bus lines view

---
+++ src/components/BusLinesList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="">
+    <div class="d-flex flex-wrap gap-2 py-3">
+      <button
+        v-for="line in busLines"
+        :key="line"
+        class="btn btn-primary line"
+        :class="{ active: selectedLine === line }"
+        @click="selectLine(line)"
+      >
+        {{ line }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue'
+
+defineProps<{
+  busLines: string[]
+}>()
+
+const emit = defineEmits(['lineSelected'])
+
+const selectedLine = ref('')
+
+const selectLine = (line: string) => {
+  selectedLine.value = line
+  emit('lineSelected', line)
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/_variables.scss';
+
+.line {
+  padding: 3px;
+  min-width: 54px;
+
+  &.active {
+    background-color: $primary-dark;
+  }
+}
+</style>
--- src/views/BusLinesView.vue
+++ src/views/BusLinesView.vue
@@ -1,8 +1,45 @@
 <template>
-  <div>
-    <h2>Select Bus Line</h2>
+  <div class="bus-lines px-4 pt-4">
+    <p class="bus-lines__header">Select Bus Line</p>
+    <BusLinesList :bus-lines="busLines" @line-selected="handleLineSelected" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+import BusLinesList from '@/components/BusLinesList.vue'
+import { BusStopTimetable, BusLine } from '@/types/BusStopTimetable'
+
+const store = useStore()
+const busStopsTimetable = computed(
+  () => store.getters.busStopsTimetable as BusStopTimetable
+)
+const busLines = computed(() => Object.keys(busStopsTimetable.value))
+
+const selectedLine = ref<keyof BusStopTimetable | null>(null)
+const selectedStop = ref<string | null>(null)
+const selectedLineStops = ref<BusLine>({})
+
+const handleLineSelected = (line: keyof BusStopTimetable) => {
+  selectedLine.value = line
+  selectedLineStops.value = selectedLine.value
+    ? busStopsTimetable.value[selectedLine.value]
+    : {}
+  selectedStop.value = null
+}
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/_variables';
+@import '../styles/_mixins';
+
+.bus-lines {
+  background-color: $white;
+
+  &__header {
+    font-size: 14px;
+    font-weight: 600;
+  }
+}
+</style>
